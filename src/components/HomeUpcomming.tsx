2836f0a255824b62385b574d1d87ec0f495155c4
Aq7853gehc <aquib7583@gmail.com>
2024-04-22 04:20:06 +0530

    end of project

--- src/components/HomeUpcomming.tsx
+++ src/components/HomeUpcomming.tsx
@@ -3,13 +3,17 @@ import { useGetCall } from "@/hooks/useGetCall";
 import { Call } from "@stream-io/video-react-sdk";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { toast, useToast } from "./ui/use-toast";
 
 const HomeUpcomming = () => {
   const { upcomingCalls } = useGetCall();
   const calls = upcomingCalls[0];
   const router =useRouter()
+  const {toast} = useToast();
   return (
-    <span onClick={() => router.push(`/meeting/${calls.id}`)} className="cursor-pointer">
+    <span onClick={calls?.id?() => router.push(`/meeting/${calls.id}`):()=>{
+      toast({title:"No Meetings"})
+    }} className="cursor-pointer">
       {`${
         calls?.state?.custom?.description?.substring(0, 20) || "No Upcomming"
       } ${calls?.state?.startsAt?.toLocaleString() || ""}`}
